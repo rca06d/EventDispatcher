3e15f07c4320b852f54a6cd927ce5b8966b7f1f6
Rob Allsopp <rallsopp@commercialtribe.com>
2015-04-21 15:58:27 -0600

    now uses native Event api

---
+++ src/eventemitter.js
@@ -0,0 +1,26 @@
+// This is a way to emulate the native dom event api on any js object
+function EventEmitter ()
+{
+	var listeners = [];
+
+	this.addEventListener = function (type, fn)
+	{
+		listeners.push({type: type, fn: fn});
+	};
+
+	this.removeEventListener = function (type, fn)
+	{
+		for ( var i = 0, l = listeners.length; i < l; i++)
+		{
+			if (listeners[i].type == type && listeners[i].fn == fn) return listeners.splice(i, 1);
+		}
+	};
+
+	this.dispatchEvent = function (e)
+	{
+		for ( var i = 0, l = listeners.length; i < l; i++)
+		{
+			if (listeners[i].type == e.type) listeners[i].fn(e);
+		}
+	};
+}
